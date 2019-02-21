3f233b519298296dfc9c01aed05e7767f2b595df
Jason Kim <jasonkim4201@gmail.com>
2019-02-20 22:11:58 -0500

    added 3 get request functions

--- public/assets/javascript/foodinput.js
+++ public/assets/javascript/foodinput.js
@@ -45,30 +45,37 @@ $(document).ready(function () {
     switch (viewParams) {
       case "all":
         console.log("view all the stuff");
+        viewAll();
         break;
 
       case "homeCook":
         console.log("home");
+        viewHomeCook();
         break;
 
       case "orderedOut":
         console.log('Stop eating out');
+        viewOrderedOut();
         break;
 
       case "vegetarian":
         console.log("veggies are good for u");
+        viewVegetarian();
         break;
 
       case "meaty":
         console.log("meaty!");
+        viewMeaty();
         break;
 
       case "leftovers":
         console.log("More food 4 tomorrow");
+        viewLeftovers();
         break;
 
       case "devoured":
         console.log("all the stuff u devoured");
+        viewDevoured();
         break;
 
       default:
@@ -77,16 +84,57 @@ $(document).ready(function () {
     }
   });
 
-// FUNCTION ZONES FOR THE SWITCH CASES
+  // FUNCTION ZONES FOR THE SWITCH CASES
 
-// 
-const viewAll = () => {
+  const viewAll = () => {
 
+    // make ajax call to view all
+    $.ajax({
+        url: "/api/mealsEaten",
+        method: "GET"
+      })
+      .then(foodResponse => {
+        // once get request is complete, empty #viewEatenFoods div and add in all listed items
+
+        $("#viewEatenFoods").empty();
+
+        foodResponse.forEach((item, index) => {
+          // add each listed meal in a <li class="list-group-item"> tag and append it to #viewEatenFoods
+          const $li = $(`<li class='list-group-item list-group-item-action list-group-item-info text-center font-weight-bold' id=${index + 1}>`);
+          $("#viewEatenFoods").append($li.text(item.meal));
+        });
+      });
+  }
+
+const viewHomeCook = () => {
+  $.ajax({
+    url: "api/homeCooked",
+    method: "GET"
+  })
+  .then(homeCookResponse => {
+    $("#viewEatenFoods").empty();
+
+    homeCookResponse.forEach((food, index) => {
+      const $li = $(`<li class='list-group-item list-group-item-action list-group-item-success text-center font-weight-bold' id=${index + 1}>`);
+      $("#viewEatenFoods").append($li.text(food.meal));
+    });
+  });
 }
 
+const viewOrderedOut = () => {
+  $.ajax({
+    url: "api/orderedOut",
+    method: "GET"
+  })
+  .then(orderedResponse => {
+    $("#viewEatenFoods").empty();
 
-
-
+    orderedResponse.forEach((food, index) => {
+      const $li = $(`<li class='list-group-item list-group-item-action list-group-item-danger text-center font-weight-bold' id=${index + 1}>`);
+      $("#viewEatenFoods").append($li.text(food.meal));
+    });
+  });
+}
 
 
 
